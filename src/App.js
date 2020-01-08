4b9eca270a678060a93f9eb85346d2a396b13fe6
Gregor Ahcin <bubi.grega@gmail.com>
2020-01-08 21:36:17 +0100

    test usereducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + action.amount };
+    case "DECREMENT":
+      return { count: state.count - action.amount };
+    case "RESET":
+      return { count: 0 };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(countReducer, { count: 5 });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{state.count}</h3>
+      <button onClick={() => dispatch({ type: "INCREMENT", amount: 5 })}>
+        add 5
+      </button>
+      <button onClick={() => dispatch({ type: "INCREMENT", amount: 1 })}>
+        add 1
+      </button>
+      <button onClick={() => dispatch({ type: "DECREMENT", amount: 5 })}>
+        decrement 5
+      </button>
+      <button onClick={() => dispatch({ type: "RESET", amount: 5 })}>
+        reset
+      </button>
     </div>
   );
 }
